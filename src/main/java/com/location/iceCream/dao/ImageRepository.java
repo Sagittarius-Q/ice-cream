79c86a18f3760f2144a8cfa5bd6f8f9f9d079fa9
Abduahad <ahmedov.abduahad@gmail.com>
2021-01-23 16:51:46 +0600

    modified

--- src/main/java/com/location/iceCream/dao/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.location.iceCream.dao;
-
-import com.location.iceCream.model.entity.ImageModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageRepository extends JpaRepository<ImageModel,Long> {
-}
